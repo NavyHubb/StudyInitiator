cebb04e1700b02f3740b82d60b427b6083e030f9
truningkick <jmj6352@naver.com>
2023-07-30 22:01:27 +0900

    feat: BOJ_5014_스타트링크 문제풀이

---
+++ truningkick/week_01/BOJ_5014_스타트링크.java
@@ -0,0 +1,12 @@
+package turningkick.week_01;
+
+
+/*
+ * 건물:F층 / 강호현위치:S층 / 스타트링크:G층 / 
+ * 선택지는 두가지,  U:위로U / D:아래D층 *층이 없을땐 이동 x /
+ * 엘리베이터로 이동 불가 시 "use the stairs" 출력
+ * inpujt : F S G U D 
+ */
+public class BOJ_5014_스타트링크 {
+
+}
