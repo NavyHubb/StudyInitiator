f80c20b3513a13db555b39fce0cee42140a55eb5
Navy <sngjae972@gmail.com>
2023-08-01 10:57:59 +0900

    fix: 로직 수정 및 주석 추가

--- NavyHubb/week_02/BOJ_10971_외판원순회2.java
+++ NavyHubb/week_02/BOJ_10971_외판원순회2.java
@@ -3,70 +3,56 @@ package NavyHubb.week_02;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class BOJ_10971_외판원순회2 {
 
-    public static int N;
-    public static int[][] wage;
-    public static int[] arr;
-    public static boolean[] visit;
-    public static int min = Integer.MAX_VALUE;
+    static int N;
+    static int[][] wage;
+    static boolean[] visited;
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        wage = new int[N][N];
-        arr = new int[N];
-        visit = new boolean[N];
 
-        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        wage = new int[N][];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                wage[i][j] = Integer.parseInt(st.nextToken());
-            }
+            wage[i] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt).toArray();
         }
 
         for (int i = 0; i < N; i++) {
-            visit = new boolean[N];
-            visit[i] = true;
-            dfs(i, i, 0);
+            visited = new boolean[N];
+            visited[i] = true;
+            dfs(i, i, 0, 0);
         }
 
         System.out.println(min);
     }
 
     /**
-     * @param start : 순회를 시작한 도시
-     * @param now : 현재 위치한 도시
-     * @param cost : 현재까지의 비용 누적합
+     * @param start - 시작 노드
+     * @param now - 현재 노드
+     * @param cost - 현재 노드까지의 이동에 소비된 누적 비용
+     * @param visitCnt - 현재 노드까지 방문한 노드 개수
      */
-    public static void dfs(int start, int now, int cost) {
-        if (allVisit()) {
+    static void dfs(int start, int now, int cost, int visitCnt) {
+        // 모든 노드를 방문한 경우
+        if (visitCnt == N-1) {
             if (wage[now][start] != 0) {
-                min = Math.min(min, cost + wage[now][0]);
+                min = Math.min(min, cost + wage[now][0]);  // 마지막에서 처음 노드로 가는 비용 더하여 최솟값 갱신
             }
-            return;
         }
 
         for (int i = 1; i < N; i++) {
-            if (!visit[i] && wage[now][i] != 0) {
-                visit[i] = true;
-                dfs(start, i, cost + wage[now][i]);
-
-                visit[i] = false;
+            // 아직 방문하지 않은 노드이면서 해당 노드로 이동할 수 있는 경우
+            if (!visited[i] && wage[now][i] != 0) {
+                visited[i] = true;
+                dfs(start, i, cost + wage[now][i], visitCnt+1);
+                visited[i] = false;
             }
         }
-    }
-
-    public static boolean allVisit() {
-        for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                return false;
-            }
-        }
-        return true;
     }
 
 }
