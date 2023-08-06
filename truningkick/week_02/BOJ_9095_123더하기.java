359ba2b7b28df57b4e2a6b19d1799d16b7cd0bf0
truningkick <jmj6352@naver.com>
2023-08-06 22:24:49 +0900

    fix: BOJ_9095_123더하기 패키지 수정

--- truningkick/week_02/BOJ_9095_123더하기.java
+++ truningkick/week_02/BOJ_9095_123더하기.java
@@ -1,20 +1,17 @@
 package truningkick.week_02;
 
-import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.*;
 
+import java.util.*;
+import java.math.*;
 public class BOJ_9095_123더하기 {
 	
 	static int dp[] = new int [11];
 	
-	public static void main(String[] args) throws NumberFormatException, IOException   {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
-
+	public static void main(String[] args)   {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int t = sc.nextInt();
 		dp[1] =1; //초기 값 초기화
 		dp[2]=2;
 		dp[3]=4;
@@ -24,8 +21,18 @@ public class BOJ_9095_123더하기 {
 		}
 		
 		for(int i=0;i<t;i++) {
-			int n = Integer.parseInt(br.readLine());	
+			int n = sc.nextInt();
+			
 			System.out.println(dp[n]);
 		}
-	}	
+		
+		
+		
+		
+		
+	}
+	
 }
+ 
+
+ 
