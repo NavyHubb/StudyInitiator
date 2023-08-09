e898a751961e078b364e1ec75ce33181251be7d9
Navy <101810007+NavyHubb@users.noreply.github.com>
2023-08-09 11:40:31 +0900

    fix : 풀이 수정

--- NavyHubb/week_03/BOJ_12865_평범한배낭.java
+++ NavyHubb/week_03/BOJ_12865_평범한배낭.java
@@ -14,57 +14,21 @@ public class BOJ_12865_평범한배낭 {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        List<Item> list = new ArrayList<>();
+        int[] dp = new int[K+1];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            list.add(new Item(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
-        Collections.sort(list);
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
-//        // 2차원 dp 배열 사용 풀이
-//        int[][] dp = new int[N+1][K+1];
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 1; j <= K; j++) {
-//                Item item = list.get(i);
-//
-//                if (item.w <= j) {  // 현재 탐색하는 무게(j)의 범위 안에 현재 아이템의 무게가 있는 경우
-//                    dp[i+1][j] = Math.max(dp[i][j], dp[i][j- item.w] + item.v);
-//                } else {
-//                    dp[i+1][j] = dp[i][j];
-//                }
-//            }
-//        }
-//
-//        System.out.println(dp[N][K]);
-
-        // 1차원 dp 배열 사용 풀이
-        int[] dp = new int[K+1];
-        for (int i = 0; i < N; i++) {
-            for (int j = K; j >= 1; j--) {  // 이전 아이템을 탐색할 때의 값을 보존하기 위해 역순으로 탐색
-                Item item = list.get(i);
-
-                if (item.w <= j) {  // 현재 탐색하는 무게(j)의 범위 안에 현재 아이템의 무게가 있는 경우
-                    dp[j] = Math.max(dp[j], dp[j- item.w] + item.v);
-                }
+            // 이전 아이템을 탐색할 때의 값을 보존하기 위해 역순으로 탐색
+            // 탐색의 범위는 현재 아이템의 무게 이상으로만 시행
+            for (int j = K; j >= w; j--) {
+                dp[j] = Math.max(dp[j], dp[j- w] + v);
             }
         }
 
         System.out.println(dp[K]);
     }
-
-    static class Item implements Comparable<Item> {
-        int w;  // 무게
-        int v;  // 가치
-
-        public Item(int w, int v) {
-            this.w = w;
-            this.v = v;
-        }
-
-        @Override
-        public int compareTo(Item o) {
-            return w - o.w;
-        }
-    }
-
+    
 }
